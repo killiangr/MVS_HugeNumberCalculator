44161ef34c55b8bf66a807b7a5227b266c966734
killiangr <73704195+killiangr@users.noreply.github.com>
2020-11-06 17:02:59 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "IHMHugeNumber/ScanHugeNumber.h"
+#include "IHMHugeNumber/ShowHugeNumber.h"
+
+
+    int main(void) {
+        HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString
+        ("123456789012345678901234567890");
+        printHugeUnsignedInt(hugeNumber);
+        hugeNumber = createHugeUnsignedIntFromString
+        ("0000000123456789012345678901234567890");
+        printHugeUnsignedInt(hugeNumber);
+    }
+
