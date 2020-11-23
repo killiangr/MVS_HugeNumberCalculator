4a947b82ba091528ef0a1495a8cbacea1129d2af
killiangr <73704195+killiangr@users.noreply.github.com>
2020-11-23 10:09:55 +0100

    Update DoublyLinkedList.c

--- DataTypes/DoublyLinkedList.c
+++ DataTypes/DoublyLinkedList.c
@@ -166,12 +166,5 @@ Digit getDigitByPlaceFromStart (const DoublyLinkedList* doublyLinkedList, unsign
     return result;
 }
 
-int main(void) {
-    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString
-    ("123456789012345678901234567890");
-    printHugeUnsignedInt(hugeNumber);
-    hugeNumber = createHugeUnsignedIntFromString
-    ("0000000123456789012345678901234567890");
-    printHugeUnsignedInt(hugeNumber);
-};
+
 
